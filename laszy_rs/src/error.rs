548938c845f7797b4ad5d2dccbdfcea430b6d351
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-01-04 16:03:50 +0100

    Add custom error type

---
+++ laszy_rs/src/error.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+pub enum LaszyError {
+    IoError(io::Error),
+    LasError(String),
+    LaszyError(String)
+}
+
+impl From<las::Error> for LaszyError {
+    fn from(error: las::Error) -> Self {
+        LaszyError::LasError(error.to_string())
+    }
+}
+
+impl From<io::Error> for LaszyError {
+    fn from(error: io::Error) -> Self {
+        LaszyError::IoError(error)
+    }
+}
